ec961f47c8e004d78c884dcc52009ee556f11aed
Temirlan <119742434+toronzzo@users.noreply.github.com>
2023-04-08 17:05:09 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+//1. Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 10; i <= 20; i++) {
+        cout << i * i << endl;
+    }
+    return 0;
+}
